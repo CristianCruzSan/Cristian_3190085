decf01fb6f6396ea301a68206ef7521a4441269b
CristianCruzSan <cruz.cristian.sanan@gmail.com>
2024-08-30 22:18:04 -0600

    Tarea: funcion suma con html y js

---
+++ funcion_suma/app.js
@@ -0,0 +1,10 @@
+let num1 = prompt("Ingresa el primer número: ");
+let num2 = prompt("Ingresa el segundo número: ");
+
+function suma(num1,num2){
+    return parseInt(num1) + parseInt(num2);
+}
+
+console.log("El resultado de sumar " + num1 + " + " + num2
+    + " es: " + suma(num1,num2)
+ )
